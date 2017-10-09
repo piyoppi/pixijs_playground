dba3b4b192e95c649e37f93b04ec966035e19bd5
piyoppi <3podvator@gmail.com>
2017-10-09 12:48:06 +0900

    黒い画面が表示されるようになった

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
+//import PIXI from 'pixi.js';
+import * as pixi from 'pixi.js';
 
+//Create the renderer
+var renderer = PIXI.autoDetectRenderer(256, 256);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
 

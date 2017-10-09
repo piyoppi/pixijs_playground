89e7e887db5155470e73e75611b9c3ed44f0ed76
piyoppi <3podvator@gmail.com>
2017-10-09 13:56:17 +0900

    スプライトを表示できた

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import * as pixi from 'pixi.js';
 
 //Create the renderer
-var renderer = PIXI.autoDetectRenderer(256, 256);
+var renderer = PIXI.autoDetectRenderer(256, 256, {antialias: false, transparent: false, resolution: 1});
 
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
@@ -13,3 +13,22 @@ var stage = new PIXI.Container();
 //Tell the `renderer` to `render` the `stage`
 renderer.render(stage);
 
+renderer.backgroundColor = 0x061639;
+//viewでCanvasがとれる
+renderer.view.style.border = "1px dashed black";
+
+//リサイズするときにはautoResize = true;
+renderer.autoResize = true;
+renderer.resize(512, 512);
+
+//Use Pixi's built-in `loader` object to load an image
+PIXI.loader
+.add("img/chara.png")
+.load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+    var chara = new PIXI.Sprite(PIXI.loader.resources["img/chara.png"].texture);
+    stage.addChild(chara);
+    renderer.render(stage);
+}

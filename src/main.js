1ef82b2fd38df0c104ac7b39124a335480722161
piyoppi <3podvator@gmail.com>
2017-10-09 14:24:34 +0900

    テクスチャにエイリアスがつけられるようになった

--- src/main.js
+++ src/main.js
@@ -24,11 +24,16 @@ renderer.resize(512, 512);
 //Use Pixi's built-in `loader` object to load an image
 PIXI.loader
 .add("img/chara.png")
+.add("kame", "img/kame.png")
 .load(setup);
 
 //This `setup` function will run when the image has loaded
 function setup() {
-    var chara = new PIXI.Sprite(PIXI.loader.resources["img/chara.png"].texture);
+    var texture = PIXI.loader.resources["img/chara.png"].texture;
+    var chara = new PIXI.Sprite(texture);
+    var kamechara = new PIXI.Sprite(PIXI.loader.resources.kame.texture);
     stage.addChild(chara);
+    stage.addChild(kamechara);
     renderer.render(stage);
+    setTimeout( ()=>{ chara.texture = PIXI.loader.resources.kame.texture; console.log("hogehoge"); renderer.render(stage); }, 2001 );
 }

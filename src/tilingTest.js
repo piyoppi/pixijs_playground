808f0640797959be5055f9e782cc3f4cdbb5c3e9
piyoppi <3podvator@gmail.com>
2017-12-26 00:57:29 +0900

    タイルスプライトのお試しコードをかいた

--- src/tilingTest.js
+++ src/tilingTest.js
@@ -1,26 +1,27 @@
 import * as pixi from 'pixi.js';
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
 //Create the renderer
-var renderer = PIXI.autoDetectRenderer(256, 256, {antialias: false, transparent: false, resolution: 1});
+var renderer = PIXI.autoDetectRenderer(512, 512, {antialias: false, transparent: false, resolution: 1});
+renderer.backgroundColor = 0xfff;
 
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
 
 //Create a container object called the `stage`
 var stage = new PIXI.Container();
+stage.x = 0;
+stage.y = 0;
 
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+PIXI.loader.add("img/chip.png")
+.add("img/chip2.png")
+.load(setup);
 
-PIXI.loader.add("img/chara.png")
-
-let tex = PIXI.Texture.from("img/chip.bmp");
-PIXI.Texture.addToCache(tex, "chip");
 //This `setup` function will run when the image has loaded
 function setup() {
-    var texture = PIXI.loader.resources["img/chara.png"].texture;
+    var texture = PIXI.loader.resources["img/chip.png"].texture;
+    var texture2 = PIXI.loader.resources["img/chip2.png"].texture;
     var chara = new PIXI.Sprite();
-    var kamechara = new PIXI.Sprite(PIXI.loader.resources.kame.texture);
     //------------------------------------------------------------------------
     //タイリングスプライト
     var tilingSprite = new PIXI.extras.TilingSprite(
@@ -28,8 +29,22 @@ function setup() {
             300,
             300 
             );
-    tilingSprite.y = 300;
+    texture.frame = new PIXI.Rectangle(64, 96, 32, 32);
+    tilingSprite.tilePosition.y = 0
+    tilingSprite.y = 1;
+    tilingSprite.x = 2;
     stage.addChild(tilingSprite);
+
+    var tilingSprite2 = new PIXI.extras.TilingSprite(
+            texture2, 
+            300,
+            300 
+            );
+    texture2.frame = new PIXI.Rectangle(0, 0, 32, 32);
+    tilingSprite2.tilePosition.y = 1
+    tilingSprite2.y = 125;
+    tilingSprite2.x = 100;
+    stage.addChild(tilingSprite2);
 
     renderer.render(stage);
 }
